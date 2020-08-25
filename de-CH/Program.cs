268823f4cb8720ab24ece91079c5d4c5638291d7
Jason Pan <v-pajaso@microsoft.com>
2020-08-25 11:51:38 +0800

    Add project files.

---
+++ de-CH/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace de_CH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the current UI culture to de-CH.
+            CultureInfo.CurrentUICulture = new CultureInfo("de-CH", false);
+            Console.WriteLine("CurrentUICulture is now {0}.", CultureInfo.CurrentUICulture.CultureTypes);
+            NumberFormatInfo nfi = CultureInfo.CurrentUICulture.NumberFormat;
+            Console.WriteLine(nfi.NumberGroupSeparator);
+            Console.WriteLine(((int)(nfi.NumberGroupSeparator.Single())).ToString("X4"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testcoreweb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using testcoreweb.Models;
+
+namespace testcoreweb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de-CH");
+            var b= Thread.CurrentThread.CurrentCulture.CultureTypes;
+            ViewBag.a = ((int)(Thread.CurrentThread.CurrentCulture.NumberFormat.NumberGroupSeparator.Single())).ToString("X4");
+            ViewBag.b = b;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
